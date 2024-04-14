a714cbbab2da3f935506f4f728cdf5dd3426438a
Patrick Cartlidge <patrickcartlidge@gmail.com>
2024-04-14 21:44:15 +0100

    eject, no chunk

--- src/index.js
+++ src/index.js
@@ -6,23 +6,25 @@ import Tabs from './components/Tabs';
 import Channels from './components/Channels';
 import Info from './components/Info';
 
-window.renderGarden = (data) => {
+window.renderGarden = (data, urlPrefix) => {
+  let rootUrl = urlPrefix || '';
+
   ReactDOM.render(
     <StrictMode>
       <Tabs tabs={[
         {
           name: 'Refs',
-          url: '/',
+          url: `${rootUrl}/`,
           scroll: true
         },
         {
           name: 'Livefeed',
-          url: '/livefeed',
+          url: `${rootUrl}/livefeed`,
           scroll: false
         },
         {
           name: 'About',
-          url: '/about',
+          url: `${rootUrl}/about`,
           scroll: true
         }
       ]}>
